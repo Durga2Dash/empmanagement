05f2e99e506805fc22712b58366b76fc31e00128
amitsgdps <amitsgdps@gmail.com>
2020-03-30 23:14:47 +0530

    second commit

--- src/main/java/com/employee/management/empmanagement/web/EmployeeMvcController.java
+++ src/main/java/com/employee/management/empmanagement/web/EmployeeMvcController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,9 +24,12 @@ public class EmployeeMvcController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Value("${spring.data.endpoint.url}")
+    String connectionURL;
+
     @RequestMapping
     public String getAllEmployees(Model model) {
-        List<EmployeeEntity> list = (List<EmployeeEntity>) restTemplate.getForObject("http://localhost:8011/data", List.class);
+        List<EmployeeEntity> list = (List<EmployeeEntity>) restTemplate.getForObject(connectionURL + "/data", List.class);
         System.out.println("[getAllEmployees] response : " + list);
         model.addAttribute("employees", list);
         return "list-employees";
@@ -35,8 +39,8 @@ public class EmployeeMvcController {
     public String editEmployeeById(Model model, @PathVariable("id") Optional<Long> id)
             throws RecordNotFoundException {
         if (id.isPresent()) {
-            System.out.println("[editEmployeeById] Id : "+id);
-            ResponseEntity<EmployeeEntity> entity = restTemplate.getForEntity("http://localhost:8011/data/edit/" + id.get(), EmployeeEntity.class);
+            System.out.println("[editEmployeeById] Id : " + id);
+            ResponseEntity<EmployeeEntity> entity = restTemplate.getForEntity(connectionURL + "/data/edit/" + id.get(), EmployeeEntity.class);
             System.out.println("[editEmployeeById] response : " + entity.getBody() + " , status code : " + entity.getStatusCode());
             model.addAttribute("employee", entity.getBody());
         } else {
@@ -48,27 +52,27 @@ public class EmployeeMvcController {
     @RequestMapping(path = "/delete/{id}")
     public String deleteEmployeeById(Model model, @PathVariable("id") Long id)
             throws RecordNotFoundException {
-        System.out.println("[deleteEmployeeById] Id : "+id);
+        System.out.println("[deleteEmployeeById] Id : " + id);
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<String> reqEntity = new HttpEntity<String>(headers);
-        ResponseEntity<String> resEntity = restTemplate.exchange("http://localhost:8011/data/delete/" + id, HttpMethod.DELETE, reqEntity, String.class);
+        ResponseEntity<String> resEntity = restTemplate.exchange(connectionURL + "/data/delete/" + id, HttpMethod.DELETE, reqEntity, String.class);
         System.out.println("[deleteEmployeeById] response : " + resEntity.getBody() + " , status code : " + resEntity.getStatusCode());
         return "redirect:/";
     }
 
     @RequestMapping(path = "/createEmployee", method = RequestMethod.POST)
     public String createOrUpdateEmployee(EmployeeEntity employee) {
-        System.out.println("[createOrUpdateEmployee] employee : "+employee);
+        System.out.println("[createOrUpdateEmployee] employee : " + employee);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<EmployeeEntity> reqEntity = new HttpEntity<EmployeeEntity>(employee, headers);
-        //ResponseEntity<String> resEntity = restTemplate.exchange("http://localhost:8011/data/create-update", HttpMethod.POST, reqEntity, String.class);
+        //ResponseEntity<String> resEntity = restTemplate.exchange(connectionURL+"/data/create-update", HttpMethod.POST, reqEntity, String.class);
 
 
         ResponseEntity<String> resEntity = restTemplate.postForEntity(
-               "http://localhost:8011/data/create-update", reqEntity , String.class);
+                connectionURL + "/data/create-update", reqEntity, String.class);
 
         System.out.println("[createOrUpdateEmployee] response : " + resEntity.getBody() + " , status code : " + resEntity.getStatusCode());
         return "redirect:/";
